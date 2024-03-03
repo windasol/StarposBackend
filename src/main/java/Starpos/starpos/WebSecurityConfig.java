a5d342bb60641b2559a0b9f3698d4aaa27cfcecb
shindongmin <vvslc77@naver.com>
2024-03-03 19:19:28 +0900

    spring secuirty 추가

---
+++ src/main/java/Starpos/starpos/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package Starpos.starpos;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+	
+
+}
---
+++ src/main/java/Starpos/starpos/entity/Users.java
@@ -0,0 +1,64 @@
+package Starpos.starpos.entity;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Users implements UserDetails {
+	@Id
+	private int userSeq;
+	private String userId;
+	private String userPw;
+	private String email;
+	private int phone;
+	private String name;
+	private boolean isLogin;
+	private String status;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+}
+
+
