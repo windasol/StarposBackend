4522b2e6a456a7e3d200a10655e89be0a231652d
dongMin <vvslc77@naver.com>
2024-03-17 21:50:11 +0900

    sdfsdf

--- src/main/java/Starpos/starpos/controller/ItemController.java
+++ src/main/java/Starpos/starpos/controller/ItemController.java
@@ -33,7 +33,6 @@ public class ItemController {
 	@GetMapping("/search-equip")
 	public List<EquipInfoDto> searchEquip(@RequestParam(value= "userId") String userId) {			
 			
-			
 		return equipInfoServiceImpl.searchEquip(userId);		
 	}
 	
